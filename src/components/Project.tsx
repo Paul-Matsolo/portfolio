c1e3b877c360461d36c7cf431b749a4ed5eea7a7
Paul-Matsolo <matsolopaulus@gmail.com>
2025-07-29 15:02:30 +0200

    Update Project.tsx

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -5,12 +5,17 @@ import '../assets/styles/Project.scss';
 function Project() {
     return(
     <div className="projects-container" id="projects">
-        <h1>Personal Projects</h1>
+        <h1>Job Simulations</h1>
         <div className="projects-grid">
             <div className="project">
-                <a href="https://www.filmate.club/" target="_blank" rel="noreferrer"><img src={mock10} className="zoom" alt="thumbnail" width="100%"/></a>
-                <a href="https://www.filmate.club/" target="_blank" rel="noreferrer"><h2>Filmate AI</h2></a>
-                <p>Developed movie finder app with semantic search and sentiment analysis using OpenAI GPT-3.5 Turbo, Qdrant, React, and Flask.</p>
+                <a href="https://www.filmate.club/" target="_blank" rel="noreferrer"><img src="https://www.theforage.com/_next/image?url=https%3A%2F%2Fcdn.theforage.com%2Fvinternships%2Fcompanyassets%2FSKZxezskWgmFjRvj9%2FBqF6gmrmLunCkdqKM%2F1691493825727%2F01-bcgx-logo-color-positive-RGB-small.png&w=1920&q=75"/></a>
+                <a href="https://www.filmate.club/" target="_blank" rel="noreferrer"><h2>BCG: Data Science</h2></a>
+                <p> Completed a customer churn analysis simulation for XYZ Analytics, demonstrating advanced data analytics skills, identifying essential client data and outlining a strategic investigation approach.
+                    Conducted efficient data analysis using Python, including Pandas and NumPy.Employed data visualization techniques for insightful trend interpretation. Completed the engineering and optimization of a random forest model, achieving an 50% recall rate in predicting customer churn.
+ Completed a concise executive summary for the team, delivering actionable
+ insights for informed decision-making based on the analysis.
+                
+                </p>
             </div>
     </div>
         </div>
