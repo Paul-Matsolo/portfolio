62b652ae6867e1953a3e9aaef259e2d15fe53ca3
Paul-Matsolo <matsolopaulus@gmail.com>
2025-07-29 22:40:45 +0200

    Update Project.tsx

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -7,8 +7,7 @@ function Project() {
         <h1>Job Simulations</h1>
         <div className="projects-grid">
             <div className="project">
-                <a href="https://www.filmate.club/" target="_blank" rel="noreferrer"><img src="https://cdn.theforage.com/vinternships/companyassets/SKZxezskWgmFjRvj9/BqF6gmrmLunCkdqKM/1691493825727/01-bcgx-logo-color-positive-RGB-small.png"alt="BCG" width="100%"
-/></a>
+                <a href="https://www.filmate.club/" target="_blank" rel="noreferrer"></a>
                 <a href="https://www.filmate.club/" target="_blank" rel="noreferrer"><h2>BCG: Data Science</h2></a>
                 <p> Completed a customer churn analysis simulation for XYZ Analytics, demonstrating advanced data analytics skills, identifying essential client data and outlining a strategic investigation approach.
                     Conducted efficient data analysis using Python, including Pandas and NumPy.Employed data visualization techniques for insightful trend interpretation. Completed the engineering and optimization of a random forest model, achieving an 50% recall rate in predicting customer churn.
